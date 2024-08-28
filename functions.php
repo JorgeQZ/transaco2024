5a50f1e2fc245cd72cde5e705fdaeb7f2c1905d2
Javier Treviño <javiertrevino@iMac-de-Javi.local>
2024-08-28 14:51:45 -0600

    Se termino header

--- functions.php
+++ functions.php
@@ -1,10 +1,15 @@
 <?php
 
-function transaco_menus()
-{
+function transaco_menus() {
     register_nav_menus(array(
         'header' => __('Header', 'Transaco 2024')
     ));
 }
 
 add_action('init', 'transaco_menus');
+
+function add_theme_scripts() {
+    wp_enqueue_style( 'generals', get_template_directory_uri(  ). '/css/generals.css');
+}
+
+add_action( 'wp_enqueue_scripts', 'add_theme_scripts');
--- header.php
+++ header.php
@@ -4,20 +4,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+
 </head>
 
 <body>
 
     <header>
 
-        <?php
-        $args = array(
-            "theme_location" => "header",
-            "container" => "nav",
-            "container_class" => "header"
-        );
-        wp_nav_menu($args);
-        wp
-        ?>
+        <div class="contenedor-header">
+            <img src="<?php echo get_template_directory_uri(); ?>/imgs/Logo owens.png" alt="Logo Owens">
+
+            <!-- Botón de menú hamburguesa -->
+            <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                &#9776;
+            </button>
+
+            <div class="menu-web">
+                <?php
+                $args = array(
+                    "theme_location" => "header",
+                    "container" => "nav",
+                    "container_class" => "header"
+                );
+                wp_nav_menu($args);
+                ?>
+            </div>
+        </div>
+
+        <div class="menu-movil">
+            <?php
+            $args = array(
+                "theme_location" => "header",
+                "container" => "nav",
+                "container_class" => "header"
+            );
+            wp_nav_menu($args);
+            ?>
+        </div>
 
     </header>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const mobileMenu = document.querySelector('.menu-movil');
+
+        menuToggle.addEventListener('click', function() {
+            menuToggle.classList.toggle('active');
+
+            if (mobileMenu.classList.contains('show')) {
+                mobileMenu.classList.remove('show');
+                setTimeout(() => {
+                    mobileMenu.style.display = 'none';
+                }, 500); // Espera a que termine la transición
+            } else {
+                mobileMenu.style.display = 'block';
+                setTimeout(() => {
+                    mobileMenu.classList.add('show');
+                }, 10); // Delay pequeño para permitir que display: block; tome efecto antes de añadir 'show'
+            }
+            });
+        });
+    </script>
