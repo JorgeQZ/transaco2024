60d5af956492cdcfa64e0b56a64f9889404f09f3
JorgeQZ <jorge_qzg@hotmail.com>
2024-09-12 15:41:36 -0600

    post thumbnails

--- functions.php
+++ functions.php
@@ -25,6 +25,9 @@ function themename_custom_logo_setup() {
 		'unlink-homepage-logo' => true,
 	);
 	add_theme_support( 'custom-logo', $defaults );
+
+    add_theme_support( 'post-thumbnails' );
+
 }
 
 add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
